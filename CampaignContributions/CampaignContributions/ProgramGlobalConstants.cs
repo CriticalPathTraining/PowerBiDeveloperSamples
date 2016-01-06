b5b6a009b3bd64bec538d38161b6c0a29528da4e
Ted Pattison <ted@tedpattison.net>
2016-01-06 18:18:37 -0500

    CC Updates

---
+++ CampaignContributions/CampaignContributions/ProgramGlobalConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignContributions {
+  class ProgramGlobalConstants {
+
+    public const string AzureAuthorizationEndpoint = "https://login.microsoftonline.com/common";
+    public const string PowerBiServiceResourceUri = "https://analysis.windows.net/powerbi/api";
+    public const string PowerBiServiceRootUrl = "https://api.powerbi.com/v1.0/myorg/";
+
+    public const string ClientID = "ddfd8af2-1115-4417-9290-c3792de968ba";
+    public const string RedirectUri = "https://localhost/CampaignContributionsDemo";
+
+
+  }
+}
